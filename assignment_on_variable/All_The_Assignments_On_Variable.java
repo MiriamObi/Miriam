79440b22d25c83e900979c9251dc3fa8ccfe1fa6
MiriamObi <71510905+MiriamObi@users.noreply.github.com>
2020-09-19 21:05:48 +0300

    Add files via upload

---
+++ assignment_on_variable/All_The_Assignments_On_Variable.java
@@ -0,0 +1,93 @@
+package assignment_on_variable;
+
+public class All_The_Assignments_On_Variable {
+
+	public static void main(String[] args) {
+		
+	
+		//QUESTION 1
+		int count = 10;
+		System.out.println(count);
+		
+		//QUESTION 2
+		String message= "I love my life";
+		System.out.println(message);
+		
+		//QUESTION 3
+		int num = 100;
+		double cost= 56.7;
+		System.out.println(num);
+		System.out.println(cost);
+		
+		//QUESTION 4
+		String word="Java";
+		System.out.println("Today\'s-Word-Of-The-Day is: " + word);
+		
+		//QUESTION 5
+		 String first="55";
+	     String second="123";
+		 System.out.println("first is 55 second = " + second);
+		 //OR
+		 System.out.print("first is " + first);
+		 System.out.println(" second = " + second);
+		
+		
+	     //QUESTION 6
+		 String name1="Bob";
+		 String name2="James";
+		 String name3="Herb";
+		 String name4="Betty";
+		 String name5="Jill";
+		 System.out.print(name1);
+		 System.out.print(",");
+		 System.out.print(name2);
+		 System.out.print(",");
+		 System.out.print(name3);
+		 System.out.print(",");
+		 System.out.print(name4);
+		 System.out.print(",");
+		 System.out.println(name5);
+		 
+		 
+		 
+		 //QUESTION 7
+		 int i=9;
+		 float f=33.4f;
+		 System.out.print("i = " + i);
+		 System.out.println(" f=" + f);
+		 
+		 
+		 
+		 //QUESTION 8
+		 String lan1="\"Java\"";
+		 String lan2="\"SQL\"";
+		 System.out.print("I will learn " + lan1);
+		 System.out.print(" and ");
+		 System.out.print(lan2);
+		 System.out.println(" at CybertekSchool.");
+		 
+		 //QUESTION 9
+		 byte steps=100;
+		 short miles=5000;
+		 int count1=1000_000;
+		 long population=343_445_5667L;
+		 
+		 //QUESTION 10
+		 int hour=12;
+		 int minute=24;
+		 int second0=33;
+		 String evening= "PM";
+		 System.out.print(hour);
+		 System.out.print(":");
+		 System.out.print(minute);
+		 System.out.print(":");
+		 System.out.print(second0);
+		 System.out.println(evening);
+		 
+	}
+	
+}
+		
+		 
+		 
+		 
